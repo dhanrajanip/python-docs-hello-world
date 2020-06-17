539e886250c52a8a6798326f1bd508c1e962c9af
dhanrajanip <dhanrajanip@gmail.com>
2020-06-18 04:21:12 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,17 @@
 from flask import Flask
+
+import cv2
+import os
+import json
+from datetime import datetime
+from pyzbar import pyzbar
+import imutils
+from imutils import contours
+import numpy as np
+import re
+import pytesseract
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
